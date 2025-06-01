5861122c17642f37daadcb3c835bf27c04916cdf
TanjilHaque <thisismydesign9@gmail.com>
2025-06-02 02:24:01 +0600

    Project Start

---
+++ src/commonComponents/TopBar.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const TopBar = () => {
+  return (
+    <div>
+      <div className="bg-Button text-Text">
+        <div className="container mx-auto  flex justify-between items-center">
+          <div></div>
+          <div>
+            Summer Sale For All Swim Suits And Free Express Delivery - OFF 50%!
+            <span>ShopNow</span>
+          </div>
+          <div>English</div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default TopBar;
---
+++ src/pages/HomePage.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import TopBar from '../commonComponents/TopBar'
+
+const HomePage = () => {
+  return (
+    <div>
+        <TopBar></TopBar>
+    </div>
+  )
+}
+
+export default HomePage
